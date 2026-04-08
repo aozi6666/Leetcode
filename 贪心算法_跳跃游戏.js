244f2340d435a3cd93346a6db1d306d4e66f1d2f
zhangao <2212433186@qq.com>
2026-04-08 09:15:24 +0800

    4.8

---
+++ 贪心算法_跳跃游戏.js
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=55 lang=javascript
+ * @lcpr version=30403
+ *
+ * [55] 跳跃游戏
+ */
+
+const { flattenJSON } = require("three/src/animation/AnimationUtils.js");
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    // 贪心选择: 能够通过 局部最优解 直接推导 出全局最优解
+    // 边界判断
+    if(nums.length === 0) return false;
+    // 记录 当前 能够到达的最远位置
+    let max_reach = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+        max_reach = Math.max(max_reach, i + nums[i]);
+
+        if(max_reach <= i) {
+            return false;
+        }
+    }
+
+    return max_reach >= nums.length;
+
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [2,3,1,1,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,2,1,0,4]\n
+// @lcpr case=end
+
+ */
+
