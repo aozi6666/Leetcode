fe1787a0eeccea3b37e48c84a214ab9972a3e144
zhangao <2212433186@qq.com>
2026-04-09 16:07:30 +0800

    4.9

--- 分治_快速排序.js
+++ 分治_快速排序.js
@@ -1,19 +1,48 @@
 function quickSort(arr) {
-    // 递归终止条件
-    if (arr.length <= 1) return arr;
-  
-    const pivot = arr[0]; // 基准值
+    // 递归结束条件
+    if(arr.length <= 1) return arr;
+
+    // 初始化-中枢按钮:基准值
+    const pivot = arr[0];
+    // 初始化-左右区间
     const left = [];
     const right = [];
-  
-    // 分区
-    for (let i = 1; i < arr.length; i++) {
-      if (arr[i] < pivot) {
-        left.push(arr[i]);
-      } else {
-        right.push(arr[i]);
-      }
+
+    // 遍历-分区
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i] <= pivot){
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
     }
-  
+
     return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+// 原地排序
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    if (left >= right) return;
+  
+    const pivotIndex = partition(arr, left, right);
+
+    function partition(arr, left, right) {
+        const pivot = arr[right]; // 选最后一个作为基准
+        let i = left;
+      
+        for (let j = left; j < right; j++) {
+          if (arr[j] < pivot) {
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+            i++;
+          }
+        }
+      
+        [arr[i], arr[right]] = [arr[right], arr[i]];
+        return i;
+      }
+  
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  
+    return arr;
   }
