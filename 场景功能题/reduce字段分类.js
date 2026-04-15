f35b1d25e7acc5599ae52486b512995d10b98959
zhangao <2212433186@qq.com>
2026-04-15 11:27:58 +0800

    4.15

--- 场景功能题/reduce字段分类.js
+++ 场景功能题/reduce字段分类.js
@@ -14,5 +14,17 @@ const dataList = [
 ];
 
 function reduceSort(arr){
-   
+   return arr.reduce(function(acc, cur){
+      // 取出 当前项的 country 值
+      const country = cur.country;
+
+      // 如果 acc 里还没有 country ，就先建一个空数组
+      if(!acc[country]){
+        acc[country] = [];
+      }
+
+      // 把当前项 push 到 country 值对应的数组中
+      acc[country].push(cur);
+
+   }, {})
 }
