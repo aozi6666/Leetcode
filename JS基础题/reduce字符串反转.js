2611f0a65eb140975e7d0834f55e67d9d39a9b6f
zhangao <2212433186@qq.com>
2026-04-15 11:35:30 +0800

    4.15

---
+++ JS基础题/reduce字符串反转.js
@@ -0,0 +1,20 @@
+/*
+    redeuce数组方法
+    arr.reduce(function(acc, cur, index, array){
+        ... 每轮处理逻辑
+        return acc;
+    }, [初始值])
+*/ 
+
+const str = 'hello world';
+
+function reduceReverse(str){
+    // 字符串转数组
+    strArr = Array.from(str);
+   return strArr.reduce(function(acc, cur){
+    // 每轮拼接：【当前字符 - acc】
+    return cur + acc;
+   }, '')
+}
+
+console.log(reduceReverse(str));
