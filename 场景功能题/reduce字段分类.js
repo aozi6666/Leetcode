177f023cd4346afd05cc455ed5277a1e4f1f2195
zhangao <2212433186@qq.com>
2026-04-15 11:03:11 +0800

    4.15

--- 场景功能题/reduce字段分类.js
+++ 场景功能题/reduce字段分类.js
@@ -1,6 +1,18 @@
+/*
+    redeuce数组方法
+    arr.reduce(function(acc, cur, index, array){
+        ... 每轮处理逻辑
+        return acc;
+    }, [初始值])
+*/ 
+
 const dataList = [
     { name: 'aa', country: 'China' },
     { name: 'bb', country: 'China' },
     { name: 'cc', country: 'USA' },
     { name: 'dd', country: 'EN' }
-  ];
+];
+
+function reduceSort(arr){
+   
+}
