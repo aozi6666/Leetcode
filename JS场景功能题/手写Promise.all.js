a11755eba30145b0cf8acb604fce281a7ef5d7f8
zhangao <2212433186@qq.com>
2026-04-19 20:51:59 +0800

    4.19

--- JS场景功能题/手写Promise.all.js
+++ JS场景功能题/手写Promise.all.js
@@ -1,5 +1,11 @@
-
-
+/* 
+promise.all:
+    把多个 Promise 放一起，
+    - 全部成功才返回成功结果数组。
+    - 只要有任意一个失败，整个就直接失败，失败原因是第一个被拒绝的原因。
+    
+    结果数组的顺序按你传入的顺序，不按完成先后顺序。
+*/
 function myPromiseAll(promises) {
     return new Promise((resolve, reject) => {
       if (!Array.isArray(promises)) {
