c3b42eb23ebad45dfba8be13ff70eae706fcea4c
zhangao <2212433186@qq.com>
2026-04-20 21:16:34 +0800

    4.20

---
+++ 1143.最长公共子序列.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=1143 lang=javascript
+ * @lcpr version=30403
+ *
+ * [1143] 最长公共子序列
+ */
+
+// @lc code=start
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function(text1, text2) {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "abcde"\n"ace"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "abc"\n"abc"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "abc"\n"def"\n
+// @lcpr case=end
+
+ */
+
