a3a036ff7bbcaba4a7c1835ee722918de2c5cac5
zhangao <2212433186@qq.com>
2026-04-20 21:12:22 +0800

    4.20

---
+++ 215.数组中的第K个最大元素.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=215 lang=javascript
+ * @lcpr version=30403
+ *
+ * [215] 数组中的第K个最大元素
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [3,2,1,5,6,4]\n2\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,2,3,1,2,4,5,5,6]\n4\n
+// @lcpr case=end
+
+ */
+
