34928cddcf3527ef4c7d619c62fd20d82c169bd7
zhangao <2212433186@qq.com>
2026-04-20 20:45:49 +0800

    4.20

--- JS基础题/手写instanceof.js
+++ JS基础题/手写instanceof.js
@@ -2,6 +2,11 @@
     instanceof 是用来判断：某个对象，是不是某个构造函数“造出来的”实例
     真正判断的是：
     - 构造函数的 prototype，有没有出现在对象的原型链上。
+
+    instanceof 的本质：判断右侧构造函数的 prototype 是否出现在左侧对象的原型链上。
+    实现：先拿到右侧的 prototype，
+         再通过 Object.getPrototypeOf 不断向上查找左侧对象的原型链，
+         如果找到就返回 true，直到原型链尽头 null 还没找到就返回 false
 */
 
 function myInstanceof(left, right) {
--- JS基础题/手写数组reduce.js
+++ JS基础题/手写数组reduce.js
@@ -1,34 +1,57 @@
-// 手写 call / apply 的核心思路，是把原函数临时挂载到目标对象上，然后通过对象调用这个函数，因为隐式绑定规则下，谁调用函数，this 就指向谁。调用完成后再删除这个临时属性。两者区别只是参数形式不同，call 是逐个传参，apply 是数组传参。
+// reduce 的作用是对数组每一项进行累计处理，最终归并成一个结果。
+// 实现时需要处理两种情况：
+//      - 传了初始值时，从第 0 项开始遍历；没传初始值时，默认用数组第一项作为累计值，从第 1 项开始遍历。
+//      - 如果数组为空且没有初始值，需要抛出错误。
 
+
+// 给“数组的原型对象”新增了一个方法：Array.prototype.myReduce
+// 参数：callback 函数，初始值（数组，对象，字符串,数字）
 Array.prototype.myReduce = function (callback, initialValue) {
-    // 1. this 指向调用 myReduce 的数组
+    // 1. （重要‼️）this 指向调用 myReduce方法 的外部数组
+    // arr 就是当前调用 这个方法 的数组本身
     const arr = this;
   
     // 2. 判断 callback 是否是函数
     if (typeof callback !== "function") {
       throw new TypeError(callback + " is not a function");
     }
-  
+
+    // 3. 定义变量
+    // 累计值、上一次计算结果
     let accumulator;
+    // 索引：从数组第几项开始遍历
     let startIndex;
   
-    // 3. 如果传了初始值，累计值就是初始值，从第 0 项开始遍历
+    // 3. 判断有没有传初始值
+    // arguments这个函数传入的参数：arguments.length > 1 ，调用 myReduce 时，是不是传了第二个参数
     if (arguments.length > 1) {
+      // 累计值就是初始值
       accumulator = initialValue;
+      // 从第 0 项开始遍历
       startIndex = 0;
     } else {
-      // 4. 如果没传初始值，累计值默认取数组第一项，从第 1 项开始遍历
+      // 4. 如果没传初始值
+      // 空数组，抛出错误
       if (arr.length === 0) {
         throw new TypeError("Reduce of empty array with no initial value");
       }
-  
+      
+      // 累计值默认取数组第一项
       accumulator = arr[0];
+      // 从第 1 项开始遍历（第 0 项已经被当初始值了）
       startIndex = 1;
     }
   
     // 5. 从 startIndex 开始遍历数组
     for (let i = startIndex; i < arr.length; i++) {
       // 6. 每次把上一次结果和当前项传给 callback
+      /* 
+        callback(prev, cur, index, array)
+            - prev：上一次累计结果
+            - cur：当前项
+            - index：当前下标
+            - array：原数组
+      */
       accumulator = callback(accumulator, arr[i], i, arr);
     }
   
