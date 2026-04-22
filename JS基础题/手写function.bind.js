079d15192515381d5ce30a00d1d2e320f175e215
zhangao <2212433186@qq.com>
2026-04-22 11:42:30 +0800

    4.22

--- JS基础题/手写function.bind.js
+++ JS基础题/手写function.bind.js
@@ -7,14 +7,43 @@
 */
 
 Function.prototype.myBind = function (context, ...args) {
-    // 1. this 指向调用 myBind 的原函数
+    // 1. 当前 this 就是调用 myBind 的原函数
     const fn = this;
   
-    // 2. 返回一个新函数
-    return function (...restArgs) {
-      // 3. 执行原函数，并把 this 绑定到 context
-      //    同时把两次传入的参数拼接起来
-      return fn.apply(context, args.concat(restArgs));
-    };
+    // 2. bind 只能绑定函数，做一层保护
+    if (typeof fn !== "function") {
+      throw new TypeError("myBind must be called on a function");
+    }
+  
+    // 3. 返回绑定后的函数
+    function boundFn(...restArgs) {
+      // 4. 判断是否通过 new 调用
+      //    如果是 new boundFn()，此时 this 应该指向新实例
+      //    并且要忽略传入的 context
+      const isNew = this instanceof boundFn;
+  
+      // 5. 决定真正执行时的 this
+      //    - 普通调用：this 指向 context
+      //    - new 调用：this 指向 new 创建出来的实例
+      const finalThis = isNew ? this : context;
+  
+      // 6. 执行原函数
+      //    拼接 bind 时传入的参数 + 调用时传入的参数
+      return fn.apply(finalThis, [...args, ...restArgs]);
+    }
+  
+    // 7. 处理原型链
+    //    原生 bind 返回的函数，如果被 new 调用，
+    //    新对象应当能继承原函数 prototype 上的方法
+    if (fn.prototype) {
+      boundFn.prototype = Object.create(fn.prototype);
+    }
+  
+    // 8. 可选：把 constructor 修正回来
+    //    不是必须，但更直观一些
+    boundFn.prototype.constructor = boundFn;
+  
+    return boundFn;
   };
+  
   
