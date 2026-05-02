c5a35179c1a325d0f1f69b22092db1a6d531d376
zhangao <2212433186@qq.com>
2026-05-02 10:58:47 +0800

    5.2

--- 滑动窗口_无重复字符最长字串.js
+++ 滑动窗口_无重复字符最长字串.js
@@ -1,57 +1,82 @@
-// 给定一个字符串 s ，请你找出其中不含有重复字符的 最长 子串 的长度。
+/*
+ * @lc app=leetcode.cn id=3 lang=javascript
+ * @lcpr version=30403
+ *
+ * [3] 无重复字符的最长子串
+ */
 
-// 示例 1:
+// @lc code=start
+/**
+ * @param {string} 
+ * @return {number}
+ */
 
-// 输入: s = "abcabcbb"
-// 输出: 3 
-// 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。注意 "bca" 和 "cab" 也是正确答案。
+// map基础知识
+/* 
+    map数据结构 （键-值 对 key-value）
+    常用方法：
+        1. map.has(key) 判断是否存在键key
+        2. map.get(key) 获取键key对应的值value
+        3. map.set(key, value) 设置键key对应的值value
+        4. map.delete(key) 删除键key对应的键值对
+    场景：计数优先用 Map
+*/
+var lengthOfLongestSubstring = function(s) {
+    // 解题思路： 滑动窗口 + map
 
-// 示例 2:
-// 输入: s = "bbbbb"
-// 输出: 1
-// 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+    // 初始化
+    // map（key, value）表示 字符为 key 的字符出现的次数为 value
+    let window = new Map();
+    // 滑动窗口，左右指针
+    let left = 0;
+    let right = 0;
+    // 结果数字
+    let res = 0;
 
-// 示例 3:
-// 输入: s = "pwwkew"
-// 输出: 3
-// 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
-//      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+    // 进入循环
+    while(right < s.length){
+        // 得到要进入窗口的 right 指针对应的元素
+        let c = s[right];  // key
+        // 窗口right右移
+        right++;
+        // 进行窗口的更新: key 对应的 value  的计数更新
+        window.set(c, (window.get(c) || 0) + 1);
 
-const lengthOfLongestSubstring = function(s) {
-    /*
-    Map字典(键-值对)：{key, value} 值value是索引
-    方法：Map.has(ch) 判断Map中是否存在键 ch，返回true/false
-    方法：Map.get(ch) 获取键ch的对应的值value，不存在返回undefined
-    方法：Map.set(key, value) 设置键(key,value)写入Map中
-    */
-
-    // 记录 每个字符 上一次出现的位置（key:字符，value:索引）
-   const last = new Map();
-
-   // 当前滑动窗口的左边界
-   let left = 0;
-   // 结果：最长无重复字符子串的长度
-   let res = 0;
-
-   // 遍历更新滑动窗口
-   for(let right = 0; right < s.length; right++) {
-        // 获取当前 滑动窗口右边界 字符
-        let ch = s[right];
-
-        // 如何 当前右边界字符 在窗口中有重复字符，更新左边界
-        if(last.has(ch) && last.get(ch) >= left) {
-            left = last.get(ch) + 1;
+        // 判断左窗口是否需要收缩
+        while(window.get(c) > 1){
+            // 取窗口左边界元素
+            let d = s[left];
+            // 窗口左指针右移
+            left++;
+            // 窗口的更新：key 对应的 value  的计数更新
+            window.set(d, window.get(d) - 1);
         }
 
-        // 更新/添加 当前字符 到Map
-        last.set(ch, right);
+        // 更新结果
+        res = Math.max(res, right - left);
+    }
+    return res;
+};
+// @lc code=end
 
-        // 更新最大 窗口大小（无重复字符子串的长度）
-        res = Math.max(res, right - left + 1);
-   }
-   
-   return res;
-}
+
+
+/*
+// @lcpr case=start
+// "abcabcbb"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "bbbbb"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "pwwkew"\n
+// @lcpr case=end
+
+ */
+
+
 
 console.log(lengthOfLongestSubstring("abcabcbb"));
 console.log(lengthOfLongestSubstring("bbbbb"));
