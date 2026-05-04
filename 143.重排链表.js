0ca0103eb0f76c0b2124b5935ce880d6ff461a2b
zhangao <2212433186@qq.com>
2026-05-04 12:16:48 +0800

    5.4

---
+++ 143.重排链表.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=143 lang=javascript
+ * @lcpr version=30403
+ *
+ * [143] 重排链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function(head) {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3,4,5]\n
+// @lcpr case=end
+
+ */
+
---
+++ 647.回文子串.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=647 lang=javascript
+ * @lcpr version=30403
+ *
+ * [647] 回文子串
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings = function(s) {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "abc"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "aaa"\n
+// @lcpr case=end
+
+ */
+
