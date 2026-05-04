0e055837f03aa935177ca02ea748ce63872cbae4
zhangao <2212433186@qq.com>
2026-05-04 12:18:53 +0800

    5.4

--- 20.有效的括号.js
+++ 20.有效的括号.js
@@ -11,7 +11,31 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+
+    // 括号映射
+    const map = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
+
+    for (let char of s) {
+        // 如果是右括号
+        if (map[char]) {
+            // 栈为空 或 不匹配
+            if (stack.length === 0 || stack.pop() !== map[char]) {
+                return false;
+            }
+        } else {
+            // 左括号入栈
+            stack.push(char);
+        }
+    }
+
+    // 最后栈必须为空
+    return stack.length === 0;
+
 };
 // @lc code=end
 
