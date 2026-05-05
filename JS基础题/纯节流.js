7edbb9c206afd59c2d79e354da800424e1ed8ee8
zhangao <2212433186@qq.com>
2026-05-05 17:22:26 +0800

    5.5

--- JS基础题/纯节流.js
+++ JS基础题/纯节流.js
@@ -1,11 +1,39 @@
-let lastTime = 0;
+function throttle(fn, delay = 300){
+  let lastTime = 0;
+  let timer = null;
+  // 解决竞态
+  let latestId = 0;
 
-function throttle(fn, delay) {
-  return function (...args) {
+  return function(...args){
     const now = Date.now();
-    if (now - lastTime >= delay) {
+    // 竞态保护
+    const id = ++latestId;
+
+    // 时间戳版本（leading）
+    if(now - lastTime >= delay){
       lastTime = now;
-      fn.apply(this, args);
+      fn.apply(this, [...args, id, latestId]);
+    } else {
+      // 清空定时器
+      clearTimeout(timer);
+      // 开启新定时器
+      timer = setTimeout(() => {
+        lastTime = Date.now();
+        fn.apply(this, [...args, id, latestId]);
+      }, delay - (now - lastTime));
     }
-  };
+  }
 }
+
+// 使用方法
+const result = throttle((value, id, latestId) => {
+  fetch(`/api/search?query=${encodeURIComponent(value)}`)
+    .then((res) => {
+      if(id === latestId){
+        console.log(res);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+}, 300);
