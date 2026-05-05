7f811bd0ecb902cf3d2aeb29f9bc2634ac524beb
zhangao <2212433186@qq.com>
2026-05-05 17:01:26 +0800

    5.4

--- JS场景功能题/节流.js
+++ JS场景功能题/节流.js
@@ -110,22 +110,39 @@ function createThrottledFetch(delay = 300) {
 function throttle(fn, delay = 300){
   let lastTime = 0;
   let timer = null;
+  // 解决竞态
+  let latestId = 0;
 
   return function(...args){
     const now = Date.now();
+    // 竞态保护
+    const id = ++latestId;
 
     // 时间戳版本（leading）
     if(now - lastTime >= delay){
       lastTime = now;
-      fn.apply(this, args);
+      fn.apply(this, [...args, id, latestId]);
     } else {
       // 清空定时器
       clearTimeout(timer);
       // 开启新定时器
       timer = setTimeout(() => {
         lastTime = Date.now();
-        fn.apply(this, args);
+        fn.apply(this, [...args, id, latestId]);
       }, delay - (now - lastTime));
     }
   }
 }
+
+// 使用方法
+const reslut = throttle((value, id, latestId) => {
+  fetch(`/api/search?query=${encodeURLComponent(value)}`)
+    .then((res) => {
+      if(id === latestId){
+        console.log(res.json());
+      }
+    })
+    .catch((err => {
+      console.log(err);
+    }))
+}, 300);
