244e0ab54aca5c269b417ed21829b8cc81d0c239
zhangao <2212433186@qq.com>
2026-05-14 22:07:27 +0800

    5.14

--- 347.前 K 个高频元素.js
+++ 347.前 K 个高频元素.js
@@ -12,7 +12,39 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    
+    // 哈希表 + 堆/排序
+     // 创建一个 Map 用来统计元素出现次数
+    // key: 数字
+    // value: 出现次数
+    const freqMap = new Map();
+
+    // 遍历数组，统计每个元素出现频率
+    for (const num of nums) {
+
+        // 如果当前数字已经存在，则次数 +1
+        // 如果不存在，则初始化为 0 后再 +1
+        freqMap.set(num, (freqMap.get(num) || 0) + 1);
+    }
+
+    // 将 Map 转成数组
+    // 例如：
+    // Map(3) {1 => 3, 2 => 2, 3 => 1}
+    // 转成：
+    // [[1,3], [2,2], [3,1]]
+    const entries = Array.from(freqMap.entries());
+
+    // 按照出现次数进行降序排序
+    // b[1] - a[1] 表示从大到小
+    entries.sort((a, b) => b[1] - a[1]);
+
+    // 截取前 k 个元素
+    // slice(0, k) 取前 k 项
+    const topK = entries.slice(0, k);
+
+    // 只返回数字本身
+    // 每项格式为 [数字, 次数]
+    // entry[0] 就是数字
+    return topK.map(entry => entry[0]);
 };
 // @lc code=end
 
