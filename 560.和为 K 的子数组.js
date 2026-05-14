bef20172b423fec1d06c5a7b17bd6c49e7d6692d
zhangao <2212433186@qq.com>
2026-05-14 21:58:31 +0800

    5.14

--- 560.和为 K 的子数组.js
+++ 560.和为 K 的子数组.js
@@ -12,7 +12,26 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
-    
+    // 解题思路：前缀和思想
+    // 前缀和：0 - i 的和
+    // 边界判断：
+    if(nums.length === 0) return 0;
+    // 最终个数
+    let count = 0;
+    let sum = 0;  // 当前和
+
+    // key: 前缀和为key, value: 前缀和为key的个数
+    const map = new Map();
+
+    // 必须设置
+    map.set(0, 1);
+
+    // 遍历每个元素
+    for(let num of nums){
+        // 前缀和
+        sum += num;
+    }
+
 };
 // @lc code=end
 
