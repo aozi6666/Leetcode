892b7bc6b49b6e8f82d669014fc92f7b4c6e5562
zhangao <2212433186@qq.com>
2026-05-14 22:03:42 +0800

    5.14

---
+++ 347.前 K 个高频元素.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=347 lang=javascript
+ * @lcpr version=30403
+ *
+ * [347] 前 K 个高频元素
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,1,1,2,2,3]\n2\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,1,2,1,2,3,1,3,2]\n2\n
+// @lcpr case=end
+
+ */
+
