36a916bb08899e8d234cde7b2c9e1eed87236d8e
zhangao <2212433186@qq.com>
2026-05-14 21:55:08 +0800

    5.14

---
+++ 560.和为 K 的子数组.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=560 lang=javascript
+ * @lcpr version=30403
+ *
+ * [560] 和为 K 的子数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums, k) {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,1,1]\n2\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3]\n3\n
+// @lcpr case=end
+
+ */
+
