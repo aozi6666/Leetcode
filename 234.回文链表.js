a0f3cc7dc2da155630b690586a4fbf02934fdd7b
zhangao <2212433186@qq.com>
2026-05-18 10:18:52 +0800

    5.18

--- 234.回文链表.js
+++ 234.回文链表.js
@@ -18,8 +18,52 @@
  * @return {boolean}
  */
 var isPalindrome = function(head) {
-    
-};
+    // 解题本质：快慢指针找中间 + 反转链表
+    if(head === null || head.next === null) return true;
+ 
+    // 反转链表回调
+    function reverse(head){
+     if(head === null || head.next === null) return head;
+     let prev = null;
+     let cur = head;
+     let next = head.next;
+ 
+ 
+     while(cur !== null){
+         cur.next = prev;
+         prev = cur;
+         cur = next;
+         
+         if(next !== null){
+             next = next.next;
+         }
+     }
+     return prev;
+    }
+ 
+    // 1. 快慢指针找中间节点
+    let fast = head;
+    let slow = head;
+ 
+    while(fast !== null && fast.next !== null){
+         fast = fast.next.next;
+         slow = slow.next;
+    }
+ 
+    // 2. 反转 slow 之后的链表
+    let newHead = reverse(slow);
+ 
+     // 3. 判断
+     while(newHead !== null){
+         // 判断
+         if(head.val !== newHead.val){
+             return false;
+         }
+         head = head.next;
+         newHead = newHead.next;
+     }
+     return true;
+ };
 // @lc code=end
 
 
