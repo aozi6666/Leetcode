80bdc0160ad8341b3b9b7c58e5b8836dc17c81a0
zhangao <2212433186@qq.com>
2026-05-22 10:21:12 +0800

    5.22

--- TodoList/src/components/TodoList.jsx
+++ TodoList/src/components/TodoList.jsx
@@ -1,43 +1,41 @@
-import {useState} from 'react'
+import {useState} from 'react';
 
 function TodoList(){
 
     const [text, setText] = useState('');
     const [list, setList] = useState([]);
 
+
     function addTodo(){
         // 空值判断
         if(!text || !text.trim()){
-            return alert('请输入待办事项');
+            alert('请输入待办事项');
+            return;
         }
-
-        // 添加
+        // 添加待办事项
         setList([...list, text]);
 
         // 清空输入框
-        setText('');
+        setList('');
     }
+
     return (
-    <>
-        <div className="contain">
-            {/* 输入框 */}
-            <input
-                value={text}
-                onChange={(e) => {setText(e.target.value)}}  
-                placeholder='请输入待办事项' 
-            />
-
-            {/* 添加按钮 */}
-            <button onClick={addTodo}>添加</button>
-
-            {/* 列表 */}
-            <ul>
-                {list.map((item, index) => (
-                    <li key={index}>{item}</li>
-                ))}
-            </ul>
-        </div>
-    </>)
+        <>
+            <div className="contain">
+                <input 
+                    value={text}
+                    onChange={(e) => {setText(e.target.value)}}
+                    placeholder='请输入待办事项'
+                />
+                <button onClick={addTodo}>添加</button>
+                <ul>
+                    {list.map((item, index) => {
+                        return <li key={index}>{item}</li>
+                    })}
+                </ul>
+            </div>
+        </>
+    )
 }
 
 export default TodoList;
