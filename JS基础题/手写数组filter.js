595025e24eed80614ab486a5751537fbbadc997c
zhangao <2212433186@qq.com>
2026-05-26 14:53:32 +0800

    5.26

--- JS基础题/手写数组filter.js
+++ JS基础题/手写数组filter.js
@@ -4,8 +4,26 @@
 */
 
 
+// 方法1: for循环实现
+Array.prototype.filter = function(fn){
+    // 初始化
+    const result = [];  // 结果
+    // 那个调用 filter 的数组
+    const initArr = this;
+
+    // 循环
+    for(let i = 0; i < initArr.length; i++){
+        
+    }
+
+}
+
+// 方法2: reduce实现
 Array.prototype.myFilter = function(fn){
-    return this.reduce((acc, cur, index, arr) => {
+    // this: 数组本身
+    const initArr = this;
+
+    return initArr.reduce((acc, cur, index, arr) => {
         if(fn(cur, index, arr)){
             // 满足条件
             acc.push(cur);
@@ -14,3 +32,4 @@ Array.prototype.myFilter = function(fn){
         return acc;
     }, [])
 }
+
