599da2aa506d2b32a1a4764ab05a3bba70a84e3a
zhangao <2212433186@qq.com>
2026-06-27 10:01:27 +0800

    6.27

---
+++ .claude/300.最长递增子序列.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=javascript
+ * @lcpr version=30403
+ *
+ * [300] 最长递增子序列
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [10,9,2,5,3,7,101,18]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0,1,0,3,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [7,7,7,7,7,7,7]\n
+// @lcpr case=end
+
+ */
+
