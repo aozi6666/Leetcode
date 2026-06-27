4fb8506114c161ddf83522ec9c0cf5a20b8ed95a
zhangao <2212433186@qq.com>
2026-06-27 11:32:30 +0800

    6.27

---
+++ 224.基本计算器.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=224 lang=javascript
+ * @lcpr version=30403
+ *
+ * [224] 基本计算器
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var calculate = function(s) {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "1 + 1"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// " 2-1 + 2 "\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "(1+(4+5+2)-3)+(6+8)"\n
+// @lcpr case=end
+
+ */
+
